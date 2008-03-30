e08b09d5c7eccd6fb6184e7a2b72b0cf521a9265
bjh <bjh@b4c647ed-9e14-0410-863f-89f006aab13e>
2008-03-30 21:49:57 +0000

    made avr.c work and expanded it slightly
    git-svn-id: file:///Volumes/FLASH/bjh/svn/Trunk/avr@114 b4c647ed-9e14-0410-863f-89f006aab13e

--- adc.c
+++ adc.c
@@ -1,3 +1,5 @@
+// Simple ADC example, targets ATmega168
+
 #include <avr/io.h> 
 //#include <avr/interrupt.h> 
 
@@ -12,7 +14,7 @@
 // RF: might be worth pulling a lot of this ADC code out into a library and defining all the variations for prescale, etc.
 //#define ADC_PRESCALE_MASK (BIT(ADPS2) | BIT(ADPS1) | BIT(ADPS0))  // Prescale clock by 128 // FIXME: this is wrong for my clock rate
 #define ADC_PRESCALE_MASK (BIT(ADPS1) | BIT(ADPS0))  // Prescale clock by 8 -- 1M / 8 = 125k
-
+#define ADC_CHANNEL_MASK (BIT(MUX0))  // ADC1
 
 int main(void) {
   LED(DDR) = B8(1111);
@@ -20,11 +22,11 @@ int main(void) {
 
   // Set up ADC
   bit_set(ADCSRA, ADC_PRESCALE_MASK);  // Set clock prescale
-  bit_set(ADMUX, BIT(REFS0));  // Set reference to AVCC
+  bit_set(ADMUX, BIT(REFS0));  // Set reference to AVcc
   bit_set(ADMUX, BIT(ADLAR));  // Set left-alignment for easy 8-bit reading of ADCH
-  // MUX setting for other than ADC0?
-  //bit_set(ADCSRA, BIT(ADFR));  // Set ADC to free-running mode  -- FIXME: dunno where this comes from datasheet says ADCSRB ADTS[2:0] = 0 for free-running
-  bit_clear(ADCSRB, (BIT(ADTS2) | BIT(ADTS1) | BIT(ADTS0)));
+  bit_set(ADMUX, ADC_CHANNEL_MASK); // Enable ADC channel (ADC1, in this case)
+  bit_set(DIDR0, BIT(ADC1D));  // Disable digital input buffer on the ADC pin
+  bit_set(ADCSRA, BIT(ADATE));  // Auto Trigger Enable  (default auto-trigger is free-running mode)
   bit_set(ADCSRA, BIT(ADEN));  // Enable ADC
   bit_set(ADCSRA, BIT(ADSC));  // Start conversions
 
